50c5c25f08c6cf4541bfadf8b23fac1d4631da33
Daisuke Takayama <takayama_daisuke@cocone.co.jp>
2023-04-07 18:00:09 +0900

    refactor: 💡 add tile map & test

---
+++ assets/tiles/tile_map2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="tile_map2" tilewidth="128" tileheight="128" tilecount="56" columns="8">
+ <image source="../images/tile_map2.png" width="1024" height="896"/>
+</tileset>
--- test/flame_isometric_test.dart
+++ test/flame_isometric_test.dart
@@ -3,48 +3,54 @@ import 'package:flame_isometric/flame_isometric.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() async {
+void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
 
-  final flameIsometric = await FlameIsometric.create(
-      tileMap: 'tile_map.png', tmx: 'tiles/tile_map.tmx');
+  group('Case with only one tmx file.', () {
+    late FlameIsometric flameIsometric;
 
-  group('public Getter', () {
-    test('layer Length check', () async {
-      expect(flameIsometric.layerLength, 3);
+    setUp(() async {
+      flameIsometric = await FlameIsometric.create(
+          tileMap: 'tile_map.png', tmx: 'tiles/tile_map.tmx');
     });
-    test('tiledMap tileWidth', () async {
-      expect(flameIsometric.tileWidth, 64);
-    });
-    test('tiledMap tileHeight', () async {
-      expect(flameIsometric.tileHeight, 32);
-    });
-    test('tiledMap srcTileSize', () async {
-      expect(flameIsometric.srcTileSize, Vector2(64.0, 64.0));
-    });
-  });
 
-  group('public API', () {
-    test('Convert Matrix to 1D array', () async {
-      expect(flameIsometric.getMatrixFlatten(0), [
-        0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, // ignore: line_length
-        0, 2, 0, 2, 2, 2, 28, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        0, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        0, 2, 2, 2, 48, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 50, 50, 50, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 50, 50, 2, 2,
-        -1, -1, 2, 2, 2, 2, 2, 2, 2, 50, 50, 50, 50, 50, 2, 2,
-        -1, -1, -1, -1, 2, 2, 2, 2, 2, 50, 2, 2, 50, 50, 2, 2,
-        -1, -1, -1, -1, -1, -1, -1, 2, 2, 50, 2, 2, 50, 50, 2, 2,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, 2, 50, 50, 2, 2
-      ]);
+    group('public Getter', () {
+      test('layer Length check', () async {
+        expect(flameIsometric.layerLength, 3);
+      });
+      test('tiledMap tileWidth', () async {
+        expect(flameIsometric.tileWidth, 64);
+      });
+      test('tiledMap tileHeight', () async {
+        expect(flameIsometric.tileHeight, 32);
+      });
+      test('tiledMap srcTileSize', () async {
+        expect(flameIsometric.srcTileSize, Vector2(64.0, 64.0));
+      });
+    });
+
+    group('public API', () {
+      test('Convert Matrix to 1D array', () async {
+        expect(flameIsometric.getMatrixFlatten(0), [
+          0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, // ignore: line_length
+          0, 2, 0, 2, 2, 2, 28, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          0, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          0, 2, 2, 2, 48, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+          2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 50, 50, 50, 2, 2,
+          2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 50, 50, 2, 2,
+          -1, -1, 2, 2, 2, 2, 2, 2, 2, 50, 50, 50, 50, 50, 2, 2,
+          -1, -1, -1, -1, 2, 2, 2, 2, 2, 50, 2, 2, 50, 50, 2, 2,
+          -1, -1, -1, -1, -1, -1, -1, 2, 2, 50, 2, 2, 50, 50, 2, 2,
+          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, 2, 50, 50, 2, 2
+        ]);
+      });
     });
   });
 }
