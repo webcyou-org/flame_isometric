0468f488b914ac2bbab9dec3c9f763208f51526f
panicdragon <d.takayama.web@gmail.com>
2023-04-13 01:31:40 +0900

    feat: 🎸 create CustomIsometricTileMapComponent

---
+++ lib/custom_isometric_tile_map_component.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+import 'package:flame/components.dart';
+
+class CustomIsometricTileMapComponent extends IsometricTileMapComponent {
+
+  CustomIsometricTileMapComponent(
+    super.tileset,
+    super.matrix, {
+    super.destTileSize,
+    super.tileHeight,
+    super.position,
+    super.size,
+    super.scale,
+    super.angle,
+    super.anchor,
+    super.children,
+    super.priority,
+  }) : _renderSprite = Sprite(tileset.image);
+
+  @override
+  Sprite _renderSprite;
+
+  @override
+  void render(Canvas c) {
+    _renderSprite.image = tileset.image;
+    final size = effectiveTileSize;
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < matrix[i].length; j++) {
+        final element = matrix[i][j];
+        if (element != -1) {
+          _renderSprite = tileset.getSpriteById(element);
+          final p = getBlockRenderPositionInts(j, i);
+          _renderSprite.render(
+            c,
+            position: p,
+            size: tileset.srcSize,
+          );
+        }
+      }
+    }
+  }
+}
